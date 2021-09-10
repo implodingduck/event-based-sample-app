1e00ec673512ab0b0034d934d5f11f9319f977aa
implodingduck <zsek320@gmail.com>
2021-09-10 15:14:48 -0500

    hopefully only logging add user events

--- event-api/ProcessB2CAuditHub.cs
+++ event-api/ProcessB2CAuditHub.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace event_api
 {
@@ -18,15 +19,23 @@ namespace event_api
             ILogger log)
         {
             var exceptions = new List<Exception>();
-
+            log.LogInformation("A new event hub thing has been triggered");
             foreach (EventData eventData in events)
             {
                 try
                 {
                     string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
-
+                    dynamic data = JsonConvert.DeserializeObject(messageBody);
+                    List<dynamic> records = data.records;
+                    foreach (dynamic record in records)
+                    {
+                        if ( "Add User".Equals(record?.operationName)){
+                            log.LogInformation("Found an add user operation");
+                            log.LogInformation($"C# Event Hub trigger function processed a message: {record}");
+                        }
+                    }
                     // Replace these two lines with your processing logic.
-                    log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
+                    
                     await Task.Yield();
                 }
                 catch (Exception e)
