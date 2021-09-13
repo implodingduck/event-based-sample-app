ee7a0f05ad10c31a64d4f3e7f9e55e603518f975
implodingduck <zsek320@gmail.com>
2021-09-13 09:10:48 -0500

    lets try getting Identity to string

--- frontend-api/GetAccounts.cs
+++ frontend-api/GetAccounts.cs
@@ -32,7 +32,7 @@ namespace frontend_api
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
             try
             {
-                string principal = JsonConvert.SerializeObject(claimsPrincipal?.Identity);
+                string principal = claimsPrincipal?.Identity.ToString();
                 //return new OkObjectResult("[{ \"hello\" : \"world\"}]");
                 return new OkObjectResult(principal);
             }
