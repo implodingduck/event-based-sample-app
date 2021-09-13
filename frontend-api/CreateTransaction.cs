b5e03f78ce77111c21240a0733d7d36e542a97fd
implodingduck <zsek320@gmail.com>
2021-09-13 16:37:00 -0500

    fixing imports

--- frontend-api/CreateTransaction.cs
+++ frontend-api/CreateTransaction.cs
@@ -1,12 +1,15 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace frontend_api
 {
@@ -15,14 +18,14 @@ namespace frontend_api
         [FunctionName("CreateTransaction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "transactions")] HttpRequest req,
-            [EventHub("%EventHubName%", ConsumerGroup="event-based-sample-app", Connection = "EventHubConnection")]IAsyncCollector<string> outputEvents,
+            [EventHub("%EventHubName%", Connection = "EventHubConnection")]IAsyncCollector<string> outputEvents,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             Transaction transaction = ((JObject)JsonConvert.DeserializeObject(requestBody)).ToObject<Transaction>();
-            transaction.createdTimestamp = DateTime.UtcNow;
+            transaction.creationTime = DateTime.UtcNow;
 
             string transactionString = JsonConvert.SerializeObject(transaction);
             outputEvents.AddAsync(transactionString);
