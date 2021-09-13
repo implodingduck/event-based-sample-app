0e67b948281bfd0dddb5d643b41b54b8fa171804
implodingduck <zsek320@gmail.com>
2021-09-13 14:49:42 -0500

    adding transactions

--- frontend-api/GetTransactions.cs
+++ frontend-api/GetTransactions.cs
@@ -14,7 +14,13 @@ namespace frontend_api
     {
         [FunctionName("GetTransactions")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "transactions/{accountid}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "transactions/{accountId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "%CosmosDBDatabase%",
+                collectionName: "accounts",
+                ConnectionStringSetting = "CosmosDBConnection",
+                SqlQuery = "select * from accounts a where a.accountId = {accountId}")]
+                IEnumerable<Transaction> transactions,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -29,7 +35,7 @@ namespace frontend_api
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(JsonConvert.SerializeObject(transactions));
         }
     }
 }
--- frontend-api/Models.cs
+++ frontend-api/Models.cs
@@ -6,9 +6,21 @@ namespace frontend_api
 {
     public class Account 
     {
+        public string id { get; set; }
         public string type { get; set; }
         public decimal balance { get; set; }
         public string uid { get; set; }
+
+    }
+
+    public class Transaction 
+    {
+        public string id { get; set; }
+        public decimal amount { get; set; }
+        public string  description { get; set; }
+        public string accountId { get; set; }
+
+        
 
     }
 }
