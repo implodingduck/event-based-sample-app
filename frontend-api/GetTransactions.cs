9e47dcae166ef65798223ba30e9a21af6755fc2b
implodingduck <zsek320@gmail.com>
2021-09-13 23:27:53 -0500

    lets try db name instead of collection

--- frontend-api/GetTransactions.cs
+++ frontend-api/GetTransactions.cs
@@ -21,7 +21,7 @@ namespace frontend_api
                 databaseName: "%CosmosDBDatabase%",
                 collectionName: "accounts",
                 ConnectionStringSetting = "CosmosDBConnection",
-                SqlQuery = "select * from accounts a where a.accountId = {accountId}")]
+                SqlQuery = "select * from %CosmosDBDatabase% a where a.accountId = {accountId}")]
                 IEnumerable<Transaction> transactions,
             ILogger log)
         {
